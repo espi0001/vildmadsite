7d854b4f89216957bdb981d8539fc92715aba671
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-20 13:44:34 +0100

    dasd

---
+++ js/productlist.js
@@ -0,0 +1,47 @@
+// https://irsycvwaapqcapcnbgra.supabase.co
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4
+
+const urlParams = new URLSearchParams(window.location.search)
+const urlInfo = urlParams.get('Urlinfo')
+console.log(urlInfo)
+
+fetch(` https://irsycvwaapqcapcnbgra.supabase.co?categories=${urlInfo}`, {
+  method: 'GET',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4'
+  }
+})
+  .then(response => response.json())
+  .then(dataReceived)
+
+function dataReceived (data) {
+  data.forEach(showProductList)
+}
+
+function showProductList (showProduct) {
+  const products = document.querySelector('template').content
+  const myClone = products.cloneNode(true)
+
+  myClone.querySelector('h2').textContent = showProduct.productdisplayname
+  myClone.querySelector('.productId').textContent = showProduct.id
+  myClone.querySelector('.season').textContent = showProduct.season
+  myClone.querySelector('a').href = `produkt.html?ProductId=${showProduct.id}`
+  myClone.querySelector(
+    'img'
+  ).src = `https://irsycvwaapqcapcnbgra.supabase.co/images/${showProduct.id}.webp`
+
+  myClone.querySelector('.price span').textContent = showProduct.price
+  myClone.querySelector('.discount span').textContent = showProduct.discount
+
+  if (!showProduct.discount) {
+    myClone.querySelector('.discount').remove()
+  }
+
+  if (showProduct.soldout) {
+    myClone.querySelector('article').classList.add('.soldout')
+  }
+
+  const parentElement = document.querySelector('main')
+  parentElement.appendChild(myClone)
+}
