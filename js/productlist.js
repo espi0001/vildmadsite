512cca03d88b2bee667b8417322ce1ec2dd64c67
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-20 15:52:26 +0100

    dasdw

--- js/productlist.js
+++ js/productlist.js
@@ -1,13 +1,9 @@
-// https://irsycvwaapqcapcnbgra.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4
-
 // const urlParams = new URLSearchParams(window.location.search)
 // const urlInfo = urlParams.get('Urlinfo')
 // console.log(urlInfo)
 
-const url = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv?category1=eq.Forrest`
-
-// ?categories=${urlInfo}
+// const url = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv?category1=eq.${urlInfo}`
+const url = 'https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv'
 
 fetch(url, {
   method: 'GET',
@@ -16,26 +12,28 @@ fetch(url, {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4'
   }
 })
-  .then(response => response.json())
+  .then(res => res.json())
   .then(dataReceived)
 
 function dataReceived (data) {
-  data.forEach(showProductList)
+  console.log('Her kommer data: ', data)
+
+  data.forEach(showProduct)
 }
 
-function showProductList (showProduct) {
-  const products = document.querySelector('.jsList').content
-  const myClone = products.cloneNode(true)
+function showProduct (product) {
+  const template = document.querySelector('template').content
+  const copy = template.cloneNode(true)
 
-  myClone.querySelector('.biotype h1').textContent = showProduct.biotype
-
-  myClone
-    .querySelector('.productTitle')
-    .setAttribute('href', `product.html?id=${showProduct.productId}`)
-  myClone.querySelector(
+  copy.querySelector('.productId').textContent = product.productId
+  copy.querySelector('.productTitle').textContent = product.productTitle
+  copy.querySelector(
     'img'
-  ).src = `https://irsycvwaapqcapcnbgra.supabase.co/${showProduct.productId}.webp`
+  ).src = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv?select=${product.productImg}`
 
-  const parentElement = document.querySelector('main')
-  parentElement.appendChild(myClone)
+  //   copy
+  //     .querySelector('.productTitle')
+  //     .setAttribute('href', `product.html?id=${product.productTitle}`)
+
+  document.querySelector('.jsList').appendChild(copy)
 }
