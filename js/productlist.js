c2c721f589a61b33600842d6bc20e2b8918e97af
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-20 15:00:05 +0100

    dasw

--- js/productlist.js
+++ js/productlist.js
@@ -1,11 +1,15 @@
 // https://irsycvwaapqcapcnbgra.supabase.co
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4
 
-const urlParams = new URLSearchParams(window.location.search)
-const urlInfo = urlParams.get('Urlinfo')
-console.log(urlInfo)
+// const urlParams = new URLSearchParams(window.location.search)
+// const urlInfo = urlParams.get('Urlinfo')
+// console.log(urlInfo)
 
-fetch(` https://irsycvwaapqcapcnbgra.supabase.co?categories=${urlInfo}`, {
+const url = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv`
+
+// ?categories=${urlInfo}
+
+fetch(url, {
   method: 'GET',
   headers: {
     apikey:
@@ -20,16 +24,17 @@ function dataReceived (data) {
 }
 
 function showProductList (showProduct) {
-  const products = document.querySelector('template').content
+  const products = document.querySelector('.jsList').content
   const myClone = products.cloneNode(true)
 
-  myClone.querySelector('h2').textContent = showProduct.productdisplayname
-  myClone.querySelector('.productId').textContent = showProduct.id
-  myClone.querySelector('.season').textContent = showProduct.season
-  myClone.querySelector('a').href = `produkt.html?ProductId=${showProduct.id}`
+  myClone.querySelector('.biotype h1').textContent = showProduct.biotype
+
+  myClone
+    .querySelector('.productTitle')
+    .setAttribute('href', `product.html?id=${showProduct.productId}`)
   myClone.querySelector(
     'img'
-  ).src = `https://irsycvwaapqcapcnbgra.supabase.co/images/${showProduct.id}.webp`
+  ).src = `https://irsycvwaapqcapcnbgra.supabase.co/${showProduct.productId}.webp`
 
   const parentElement = document.querySelector('main')
   parentElement.appendChild(myClone)
