697a76b15da4bcde1b1959c137776d3e7aa9be98
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-21 16:29:05 +0100

    1629

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-function highlightCurrentPage() {
-  var breadcrumbItems = document.querySelectorAll(".breadcrumb");
+function highlightCurrentPage () {
+  var breadcrumbItems = document.querySelectorAll('.breadcrumb')
 
   breadcrumbItems.forEach(function (item) {
     // Check if the href attribute matches the current page URL
-    if (item.getAttribute("href") === window.location.pathname) {
-      item.classList.add(".active");
+    if (item.getAttribute('href') === window.location.pathname) {
+      item.classList.add('.active')
     } else {
-      item.classList.remove(".active");
+      item.classList.remove('.active')
     }
-  });
+  })
 }
 
 // Initial highlighting check in case the page is loaded with a specific URL
-highlightCurrentPage();
+highlightCurrentPage()
+
+const goTop = document.querySelector('#goTop')
+
+goTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+})
