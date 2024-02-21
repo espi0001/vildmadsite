1d94e404261ae7eb371d4a681b04433929273949
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-21 12:47:06 +0100

    fas

--- js/productlist.js
+++ js/productlist.js
@@ -1,13 +1,8 @@
-// https://irsycvwaapqcapcnbgra.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc3ljdndhYXBxY2FwY25iZ3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzU5NjQsImV4cCI6MjAyMzA1MTk2NH0.rP1hLkHs5DYGMkNsIqcf_M5SjqcFRPjl45dEWrO6pW4
+const urlParams = new URLSearchParams(window.location.search)
+const urlInfo = urlParams.get('Urlinfo')
+console.log(urlInfo)
 
-// const urlParams = new URLSearchParams(window.location.search)
-// const urlInfo = urlParams.get('Urlinfo')
-// console.log(urlInfo)
-
-const url = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv`
-
-// ?categories=${urlInfo}
+const url = `https://irsycvwaapqcapcnbgra.supabase.co/rest/v1/vildereMadCsv?categories=${urlInfo}`
 
 fetch(url, {
   method: 'GET',
